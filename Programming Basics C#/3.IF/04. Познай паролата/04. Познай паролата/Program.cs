1ef3f2bc3d5181e812c75a2b2178a9b1c4d36eca
MitkoMB <mitko.m.barakov@gmail.com>
2023-01-02 19:42:54 +0200

    Uploaded the third lecture of the Programming Basics course

---
+++ Programming Basics C#/3.IF/04. Познай паролата/04. Познай паролата/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Познай_паролата
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = Console.ReadLine();
+            if (phrase=="s3cr3t!P@ssw0rd")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
---
+++ Programming Basics C#/3.IF/07. Лица на фигури/07. Лица на фигури/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _07._Лица_на_фигури
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string shape = Console.ReadLine();
+            if (shape=="square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine(a*a);
+            }
+            if (shape=="rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine(a * b);
+            }
+            if (shape=="triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double ha = double.Parse(Console.ReadLine());
+                Console.WriteLine(a*ha/2);
+            }
+            if (shape=="circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                Console.WriteLine(Math.PI*r*r);
+            }
+        }
+    }
+}
