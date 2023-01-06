a524cadbb9ec34b1475f6c8cc703a5b307770fd6
MitkoMB <mitko.m.barakov@gmail.com>
2023-01-06 09:34:23 +0200

    Uploaded sixth lecture of the Programming Basics course

---
+++ Programming Basics C#/6.IF ADV EX/01. Кино/01. Кино/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01._Кино
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            if (type=="Premiere")
+            {
+                Console.WriteLine($"{rows*cols*12.00:f2} leva.");
+            }
+            else if (type == "Normal")
+            {
+                Console.WriteLine($"{rows * cols * 7.50:f2} leva.");
+            }
+            else if (type == "Discount")
+            {
+                Console.WriteLine($"{rows * cols * 5.00:f2} leva.");
+            }
+        }
+    }
+}
---
+++ Programming Basics C#/6.IF ADV EX/02. Лятно облекло/02. Лятно облекло/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Лятно_облекло
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
