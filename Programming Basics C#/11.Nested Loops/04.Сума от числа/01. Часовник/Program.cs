e9fecde7162721c1f6e4c4b5dc454a0c1dbc00d1
MitkoMB <mitko.m.barakov@gmail.com>
2023-01-12 09:58:32 +0200

    Uploaded eleventh lecture of the Programming Basics course

---
+++ Programming Basics C#/11.Nested Loops/04.Сума от числа/01. Часовник/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Часовник
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = start; i <= end; i++)
+            {
+                for (int j = start; j <= end; j++)
+                {
+                    counter++;
+                    if (i+j==magicNum)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({i} + {j} = {magicNum})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{counter} combinations - neither equals {magicNum}");
+        }
+    }
+}
---
+++ Programming Basics C#/11.Nested Loops/05. Пътуване/05. Пътуване/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05._Пътуване
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            
+            while (destination!="End")
+            {
+                double neededMoney = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+                while (savedMoney<neededMoney)
+                {
+                    double savings = double.Parse(Console.ReadLine());
+                    savedMoney += savings;
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
