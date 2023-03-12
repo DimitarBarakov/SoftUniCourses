d86a5974d7ff789a1eb3a7b33c7d96d0bce2843b
MitkoMB <mitko.m.barakov@gmail.com>
2023-03-12 21:12:43 +0200

    Uploaded the Solid lecture from C# OOP lecture

---
+++ C# OOP/10. Solid/Lab-Skeleton/P01.Stream_Progress/IProgreseable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01.Stream_Progress
+{
+    public interface IProgreseable
+    {
+        public int BytesSent { get; set; }
+        public int Length { get; set; }
+    }
+}
