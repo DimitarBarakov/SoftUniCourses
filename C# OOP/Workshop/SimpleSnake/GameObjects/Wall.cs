2e4b14910d5f4d9114aae92c3eb8e4dca7639f27
MitkoMB <mitko.m.barakov@gmail.com>
2023-03-19 20:04:01 +0200

    Uploaded the Workshop lecture from C# OOP course

---
+++ C# OOP/Workshop/SimpleSnake/GameObjects/Wall.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSnake.GameObjects
+{
+    public class Wall : Point
+    {
+        private const char WallSymbol = '\u25A0';
+        public Wall(int leftx, int topy) : base(leftx, topy)
+        {
+            InitializeBorder();
+        }
+
+        private void SetHorizontalLine(int topY)
+        {
+            for (int leftx = 0; leftx < this.LeftX; leftx++)
+            {
+                this.Draw(leftx,topY,WallSymbol);
+            }
+        }
+        private void SetVerticalLine(int leftX)
+        {
+            for (int topy = 0; topy < this.TopY; topy++)
+            {
+                this.Draw(leftX, topy, WallSymbol);
+            }
+        }
+
+        private void InitializeBorder()
+        {
+            SetHorizontalLine(0);
+            SetHorizontalLine(this.TopY);
+
+            SetVerticalLine(0);
+            SetVerticalLine(this.LeftX - 1);
+        }
+    }
+}
