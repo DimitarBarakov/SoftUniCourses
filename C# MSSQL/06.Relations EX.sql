5799a9a166968b8b741140ebdf4376e0a894e3e7
MitkoMB <mitko.m.barakov@gmail.com>
2023-04-25 20:08:07 +0300

    Uploaded the Relations lecture from C# DB MSSQL course

---
+++ C# MSSQL/06.Relations EX.sql
@@ -0,0 +1,49 @@
+--05
+
+CREATE DATABASE OnlineStore
+CREATE TABLE ItemTypes
+(
+	ItemTypeID INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Items
+(
+	ItemID INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL,
+	ItemTypeID INT FOREIGN KEY REFERENCES ItemTypes(ItemTypeID)
+)
+
+
+CREATE TABLE Cities
+(
+	CityID INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Customers
+(
+	CustomerID INT PRIMARY KEY,
+	[Name] NVARCHAR(50) NOT NULL,
+	Birthday DATETIME2 NOT NULL,
+	CityID INT FOREIGN KEY REFERENCES Cities(CityID)
+)
+
+CREATE TABLE Orders
+(
+	OrderID INT PRIMARY KEY,
+	CustomerID INT FOREIGN KEY REFERENCES Customers(CustomerID)
+)
+
+CREATE TABLE OrderItems
+(
+	OrderID INT FOREIGN KEY REFERENCES Orders(OrderID),
+	ItemID INT FOREIGN KEY REFERENCES Items(ItemID),
+	PRIMARY KEY (OrderID, ItemID)
+)
+
+--09
+SELECT m.MountainRange, p.PeakName, p.Elevation FROM Mountains AS m
+JOIN  Peaks AS p ON p.MountainId = m.Id
+WHERE m.Id = 17
+ORDER BY p.Elevation DESC
