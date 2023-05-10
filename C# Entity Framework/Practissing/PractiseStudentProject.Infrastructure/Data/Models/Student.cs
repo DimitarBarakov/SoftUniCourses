e9c350927c4520afca64894d128eab42c3ba4e88
MitkoMB <mitko.m.barakov@gmail.com>
2023-05-10 21:23:25 +0300

    Uploaded the Practissing lecture from C# Entity Framework course

---
+++ C# Entity Framework/Practissing/PractiseStudentProject.Infrastructure/Data/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseStudentProject.Infrastructure.Data.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        public int Number { get; set; }
+    }
+}
---
+++ C# Entity Framework/Practissing/PractiseStudentProject.Infrastructure/Data/PractiseStudentProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PractiseStudentProject.Infrastructure.Data.Models;
+
+namespace PractiseStudentProject.Infrastructure.Data
+{
+    public class PractiseStudentProjectContext : DbContext
+    {
+        public PractiseStudentProjectContext()
+        {
+            
+        }
+        public PractiseStudentProjectContext(DbContextOptions<PractiseStudentProjectContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } = null!;
+    }
+}
